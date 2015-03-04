9bd8353e1dea577a033ee3c05124ee3e878f06e5
unknown <mathavan@vedaconsulting.co.uk>
2015-03-04 12:44:11 +0000

    fix alignmet

--- CRM/Pcpteams/Utils.php
+++ CRM/Pcpteams/Utils.php
@@ -2,18 +2,18 @@
 
 /**
  * FIXME
- * Pcp Utils, 
+ * Pcp Utils,
  * to call and use the methods in all pcp forms / pages.
  */
 
 class  CRM_Pcpteams_Utils {
-	//Constants
-	CONST C_PCP_CUSTOM_GROUP_NAME = 'PCP_Custom_Set',
-				C_CUSTOM_GROUP_EXTENDS	= 'PCP';
-	/**
-	 * to get the logged in User Id
-	 */
-	function getloggedInUserId(){
+  //Constants
+  CONST C_PCP_CUSTOM_GROUP_NAME = 'PCP_Custom_Set',
+        C_CUSTOM_GROUP_EXTENDS	= 'PCP';
+  /**
+   * to get the logged in User Id
+   */
+  function getloggedInUserId(){
     $session    = CRM_Core_Session::singleton( );
     $contactID  = $session->get('userID'        );
     return $contactID;
