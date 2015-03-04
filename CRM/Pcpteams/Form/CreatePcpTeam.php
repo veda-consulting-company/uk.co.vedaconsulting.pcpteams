839c8a4a109526b1ccac596fd24a22ea97a57df8
unknown <mathavan@vedaconsulting.co.uk>
2015-03-04 19:06:45 +0000

    create form pcp team post process, form rule

--- CRM/Pcpteams/Form/CreatePcpTeam.php
+++ CRM/Pcpteams/Form/CreatePcpTeam.php
@@ -8,11 +8,18 @@ require_once 'CRM/Core/Form.php';
  * @see http://wiki.civicrm.org/confluence/display/CRMDOC43/QuickForm+Reference
  */
 class CRM_Pcpteams_Form_CreatePcpTeam extends CRM_Core_Form {
+  CONST C_CONTACT_SUB_TYPE = 'Team';
+  function preProcess() {
+    //FIXME : get pcp Id and use Id to relate the team and pcp.
+    parent::preProcess();
+  }
+
   function buildQuickForm() {
 
     // add form elements
     $this->add('text', 'organization_name', ts('Organization Name'), array(), TRUE);
-    $this->add('text', 'Email', ts('Email'));
+    $this->add('text', 'email-primary', ts('Email'), CRM_Core_DAO::getAttribute('CRM_Core_DAO_Email', 'email'));
+
     $this->addButtons(array(
       array(
         'type' => 'submit',
@@ -23,12 +30,37 @@ class CRM_Pcpteams_Form_CreatePcpTeam extends CRM_Core_Form {
 
     // export form elements
     $this->assign('elementNames', $this->getRenderableElementNames());
+    $this->addFormRule(array('CRM_Pcpteams_Form_CreatePcpTeam', 'formRule'));
     parent::buildQuickForm();
   }
 
+  static function formRule($params) {
+    if (!empty($params['email-primary']) && !filter_var($params['email-primary'], FILTER_VALIDATE_EMAIL)) {
+      $errors['email-primary'] = ts('Not Valid Email');
+    }
+    return empty($errors) ? TRUE : $errors;
+  }
+
   function postProcess() {
-    $values = $this->exportValues();
-    //FIXME
+    $values   = $this->exportValues();
+    $orgName  = $values['organization_name'];
+    $email    = $values['email-primary'];
+    $cSubType = self::C_CONTACT_SUB_TYPE;
+
+    $params   = array(
+                'version'          => '1',
+                'contact_type'     => 'Organization',
+                'contact_sub_type' => $cSubType,
+                'organization_name'=> $orgName,
+                'api.Email.create' => $email,
+                );
+    $createTeam = civicrm_api3('Contact', 'create', $params);
+
+    //FIXME: relate the PCP and Created Team
+
+    if(!civicrm_error($createTeam)){
+      CRM_Core_Session::setStatus(ts("Team \"{$orgName}\" has Created"), '', 'success');
+    }
     parent::postProcess();
   }
 
