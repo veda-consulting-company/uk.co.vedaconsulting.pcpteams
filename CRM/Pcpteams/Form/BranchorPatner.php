a40015499472e41e70089f52fee3dddfdadc9412
unknown <mathavan@vedaconsulting.co.uk>
2015-03-04 16:26:46 +0000

    Branch or Corporate Patner

---
+++ CRM/Pcpteams/Form/BranchorPatner.php
@@ -0,0 +1,54 @@
+<?php
+
+require_once 'CRM/Core/Form.php';
+
+/**
+ * Form controller class
+ *
+ * @see http://wiki.civicrm.org/confluence/display/CRMDOC43/QuickForm+Reference
+ */
+class CRM_Pcpteams_Form_BranchorPatner extends CRM_Core_Form {
+  function buildQuickForm() {
+
+    // add form elements
+    $this->addEntityRef('pcp_team_contact', ts('Branch or Corporate Patner'), array('api' => array('params' => array('contact_type' => 'Organization')), 'create' => TRUE), TRUE);
+    $this->addButtons(array(
+      array(
+        'type' => 'submit',
+        'name' => ts('Submit'),
+        'isDefault' => TRUE,
+      ),
+    ));
+
+    // export form elements
+    $this->assign('elementNames', $this->getRenderableElementNames());
+    parent::buildQuickForm();
+  }
+
+  function postProcess() {
+    $values = $this->exportValues();
+    //FIXME
+    parent::postProcess();
+  }
+
+  /**
+   * Get the fields/elements defined in this form.
+   *
+   * @return array (string)
+   */
+  function getRenderableElementNames() {
+    // The _elements list includes some items which should not be
+    // auto-rendered in the loop -- such as "qfKey" and "buttons".  These
+    // items don't have labels.  We'll identify renderable by filtering on
+    // the 'label'.
+    $elementNames = array();
+    foreach ($this->_elements as $element) {
+      /** @var HTML_QuickForm_Element $element */
+      $label = $element->getLabel();
+      if (!empty($label)) {
+        $elementNames[] = $element->getName();
+      }
+    }
+    return $elementNames;
+  }
+}
